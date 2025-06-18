5a1fbb8a11e5fcec47d325346db619efd8ac6ac9
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-18 16:03:53 -0300

    Create default button

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  children: React.ReactNode;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  className?: string;
+}
+
+export const Button = ({
+  children,
+  onClick,
+  type = "button",
+  disabled = false,
+  className,
+}: Props) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`bg-primary py-2 px-4 rounded-lg text-sm uppercase font-semibold cursor-pointer transition-all hover:bg-dark-primary ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
