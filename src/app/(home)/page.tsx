c075df12bd2970f5e7903c5c2963491d5ded29f9
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-20 18:20:43 -0300

    Add more information in the home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,30 +4,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="relative w-screen h-screen">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="w-full h-full object-cover"
-        >
+      <div className="relative -mt-[52px] w-screen h-screen">
+        <video autoPlay muted loop playsInline className="w-full h-full object-cover">
           <source src="/videos/home-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute top-0 bottom-0 right-0 left-0 bg-black opacity-50" />
 
-        <div className="absolute bottom-1/12 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-4 m-auto px-4">
+        <div className="absolute top-1/8 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-10 m-auto px-4">
           <Image
-            src={"/images/me.jpeg"}
+            src={"/images/me.png"}
             alt={"My avatar"}
             width={500}
             height={500}
-            className="w-50 h-50 object-cover rounded-full border-6 border-light-primary"
+            className="w-30 h-30 md:w-50 md:h-50 object-cover rounded-full border-2 border-dark-primary bg-light-primary"
           />
-          <Title className="text-center bg-light-primary rounded-full py-4 px-8">
-            Welcome! I&apos;m Lucas Regalado
+          <Title className="text-center bg-light-primary border-2 border-dark-primary rounded-md md:rounded-full p-4 md:px-8">
+            <span className="hidden md:inline-block">Welcome!</span> I&apos;m Lucas Regalado
           </Title>
+          <p className="text-center bg-light-primary border-2 border-dark-primary rounded-md md:rounded-full p-4 md:px-8">
+            I&apos;m a software developer with over 8 years of experience. I&apos;m passionate about
+            my craft, enjoy working in a team, and always seek new challenges that enable me to grow
+            and help others. I invite you to explore this site, where you&apos;ll find more
+            information about who I am, my past projects, and how to contact me. Thank you for being
+            here!
+          </p>
         </div>
       </div>
       <div className="min-h-96">Hola</div>
