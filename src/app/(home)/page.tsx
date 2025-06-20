8d3bdd69c23aa2c1e15a54055902c189af008f2b
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-20 18:23:36 -0300

    Improvement on top data in the home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,18 +11,18 @@ export default function Home() {
         </video>
         <div className="absolute top-0 bottom-0 right-0 left-0 bg-black opacity-50" />
 
-        <div className="absolute top-1/8 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-10 m-auto px-4">
+        <div className="absolute top-20 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-10 m-auto px-4">
           <Image
             src={"/images/me.png"}
             alt={"My avatar"}
             width={500}
             height={500}
-            className="w-30 h-30 md:w-50 md:h-50 object-cover rounded-full border-2 border-dark-primary bg-light-primary"
+            className="w-40 h-40 md:w-50 md:h-50 object-cover rounded-full border-2 border-dark-primary bg-light-primary"
           />
-          <Title className="text-center bg-light-primary border-2 border-dark-primary rounded-md md:rounded-full p-4 md:px-8">
+          <Title className="text-center bg-light-primary border-2 border-dark-primary rounded-xl md:rounded-full p-4 md:px-8">
             <span className="hidden md:inline-block">Welcome!</span> I&apos;m Lucas Regalado
           </Title>
-          <p className="text-center bg-light-primary border-2 border-dark-primary rounded-md md:rounded-full p-4 md:px-8">
+          <p className="text-center bg-light-primary border-2 border-dark-primary rounded-xl md:rounded-full p-4 md:px-8">
             I&apos;m a software developer with over 8 years of experience. I&apos;m passionate about
             my craft, enjoy working in a team, and always seek new challenges that enable me to grow
             and help others. I invite you to explore this site, where you&apos;ll find more
