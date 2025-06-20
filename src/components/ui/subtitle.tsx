abdf8edb073165d804c22fe42c844b3e71c30144
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-20 15:52:11 -0300

    Change subtitle component for accept a children

--- src/components/ui/subtitle.tsx
+++ src/components/ui/subtitle.tsx
@@ -1,26 +1,18 @@
 import { Lora } from "next/font/google";
-import Link from "next/link";
 
 const titleFont = Lora();
 
 interface Props {
-  name: string;
+  children: React.ReactNode;
   className?: string;
-  href?: string;
 }
 
-export const Subtitle = ({ name, className, href }: Props) => {
+export const Subtitle = ({ children, className }: Props) => {
   return (
     <h2
       className={`${titleFont.className} antialiased text-2xl font-bold text-dark-primary ${className}`}
     >
-      {href ? (
-        <Link href={href} className="hover:underline">
-          {name}
-        </Link>
-      ) : (
-        name
-      )}
+      {children}
     </h2>
   );
 };
