19a8b74e72bce69c5e403393717d16c2dac97c2b
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-20 20:17:38 -0300

    Change first view on home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,19 +9,22 @@ export default function Home() {
           <source src="/videos/home-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="absolute top-0 bottom-0 right-0 left-0 bg-black opacity-50" />
+        <div className="absolute top-0 bottom-0 right-0 left-0 bg-light-primary opacity-20" />
 
-        <div className="absolute top-20 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-10 m-auto px-4">
-          <Image
-            src={"/images/me.png"}
-            alt={"My avatar"}
-            width={500}
-            height={500}
-            className="w-40 h-40 md:w-50 md:h-50 object-cover rounded-full border-2 border-dark-primary bg-light-primary"
-          />
-          <Title className="text-center bg-light-primary border-2 border-dark-primary rounded-xl md:rounded-full p-4 md:px-8">
-            <span className="hidden md:inline-block">Welcome!</span> I&apos;m Lucas Regalado
-          </Title>
+        <div className="absolute bottom-10 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-5 m-auto px-4">
+          <div className="flex gap-4 items-center">
+            <Image
+              src={"/images/me.png"}
+              alt={"My avatar"}
+              width={400}
+              height={400}
+              className="w-40 h-40 object-cover rounded-full border-2 border-dark-primary bg-light-primary"
+            />
+
+            <Title className="text-center bg-light-primary border-2 border-dark-primary rounded-xl md:rounded-full p-4 md:px-8 h-fit">
+              <span className="hidden md:inline-block">Welcome!</span> I&apos;m Lucas Regalado
+            </Title>
+          </div>
           <p className="text-center bg-light-primary border-2 border-dark-primary rounded-xl md:rounded-full p-4 md:px-8">
             I&apos;m a software developer with over 8 years of experience. I&apos;m passionate about
             my craft, enjoy working in a team, and always seek new challenges that enable me to grow
