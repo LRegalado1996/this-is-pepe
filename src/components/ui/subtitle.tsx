637ed278c109c13fbc9775bdb9de0642d30fa855
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-21 01:44:41 -0300

    Improvement on subtitle compornent

--- src/components/ui/subtitle.tsx
+++ src/components/ui/subtitle.tsx
@@ -9,9 +9,11 @@ interface Props {
 
 export const Subtitle = ({ children, className = "" }: Props) => {
   return (
-    <div className={`${className}`}>
+    <>
       <div className="h-1 w-8 bg-dark-primary mb-1" />
-      <h2 className={`${titleFont.className} antialiased text-lg font-semibold`}>{children}</h2>
-    </div>
+      <h2 className={`${titleFont.className} antialiased text-lg font-semibold ${className}`}>
+        {children}
+      </h2>
+    </>
   );
 };
