b46e01c8f151f7ff1f8cf3ba4035c89e4924e6cc
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-21 01:33:38 -0300

    Improvement on Google form component

--- src/components/contact/google-form.tsx
+++ src/components/contact/google-form.tsx
@@ -3,13 +3,13 @@ import { Subtitle } from "../ui/subtitle";
 export const GoogleFormContact = () => {
   return (
     <div>
-      <Subtitle>I have left you the following form so that you can write to me</Subtitle>
+      <Subtitle className="mb-2">Send me a message</Subtitle>
       <div className="relative h-screen w-full">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSd5_Y749yiy8U2_aS0okWBZ-jvNg9VTXt8S1uQbTq_6fGgDLA/viewform?embedded=true"
           className="absolute top-0 left-0 w-full h-full border-0"
         >
-          Cargando…
+          Loading...
         </iframe>
       </div>
     </div>
