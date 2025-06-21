70fb13ed0d4cbadaa08c0d793275879a97c29e8a
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-21 01:45:03 -0300

    Add Social networks in home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,17 @@
-import { Title } from "@/components";
+import { SocialNetworks, Title } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="relative -mt-[52px] w-screen h-screen">
+      <section className="relative -mt-[52px] w-screen h-screen mb-8">
         <video autoPlay muted loop playsInline className="w-full h-full object-cover">
           <source src="/videos/home-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute top-0 bottom-0 right-0 left-0 bg-light-primary opacity-20" />
 
-        <div className="absolute bottom-10 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-5 m-auto px-4">
+        <div className="absolute bottom-8 right-0 left-0 max-w-[1280px] flex flex-col items-center gap-8 m-auto px-4">
           <div className="flex gap-4 items-center">
             <Image
               src={"/images/me.png"}
@@ -25,6 +25,7 @@ export default function Home() {
               <span className="hidden md:inline-block">Welcome!</span> I&apos;m Lucas Regalado
             </Title>
           </div>
+
           <p className="text-center bg-light-primary border-2 border-dark-primary rounded-xl md:rounded-full p-4 md:px-8">
             I&apos;m a software developer with over 8 years of experience. I&apos;m passionate about
             my craft, enjoy working in a team, and always seek new challenges that enable me to grow
@@ -33,8 +34,11 @@ export default function Home() {
             here!
           </p>
         </div>
-      </div>
-      <div className="min-h-96">Hola</div>
+      </section>
+
+      <section className="w-full max-w-[1280px] px-4">
+        <SocialNetworks className="flex flex-col items-center" />
+      </section>
     </>
   );
 }
