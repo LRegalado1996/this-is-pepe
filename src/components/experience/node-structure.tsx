2c385794d04a70de49105bfb3a3465283eb1fa86
Lucas Regalado <lucasregalado@hotmail.com>
2025-06-23 22:49:29 -0300

    Improvement on  node-estructure component

--- src/components/experience/node-structure.tsx
+++ src/components/experience/node-structure.tsx
@@ -1,7 +1,7 @@
+import Link from "next/link";
 import { JobStructure } from "./types";
 import { Subtitle } from "../ui/subtitle";
 import { formatDateRange } from "@/utils";
-import { ChevronUp } from "lucide-react";
 
 interface Props {
   jobsStructure: JobStructure[];
@@ -10,23 +10,29 @@ interface Props {
 export const NodeStructure = ({ jobsStructure }: Props) => {
   return (
     <div className="grid grid-cols-[32px_1fr]">
-      {jobsStructure.map(({ company, startDates, endDates, experiences }, i) => [
-        <div key={"a" + i} className="flex justify-center relative w-full h-full">
+      {jobsStructure.map(({ company, startDates, endDates, experiences, skils }, i) => [
+        <div key={"node-" + i} className="flex justify-center relative w-full h-full">
           {/* Vertical line */}
           <div className="w-0.5 h-full absolute top-0 bottom-0 bg-primary" />
-          {i === 0 && (
-            <ChevronUp size={30} strokeWidth={2} className="absolute -top-3 text-primary" />
-          )}
 
           {/* Nodes */}
           <div className="w-4 h-4 rounded-full bg-dark-primary z-10 relative top-5.5" />
         </div>,
-        <div key={"b" + i} className="p-2 mb-6">
+        <div key={"job-" + i} className="p-2 mb-6">
           <Subtitle className="flex flex-wrap gap-x-2 items-baseline mb-4">
-            {company}
+            <Link href={company.url} className="hover:underline">
+              {company.name}
+            </Link>
             <small className="text-xs h-fit">{formatDateRange(startDates, endDates)}</small>
           </Subtitle>
-          <div>{experiences}</div>
+          {experiences}
+          <ul className="flex flex-wrap gap-2 mt-4">
+            {skils.map((skill, i) => (
+              <li key={i} className="bg-primary py-2 px-4 rounded-full text-sm ">
+                {skill}
+              </li>
+            ))}
+          </ul>
         </div>,
       ])}
     </div>
